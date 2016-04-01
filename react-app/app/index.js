ef114ffdd6095c6053ed661436232b28695f4714
Guilherme Toti <guilhermetoti@gmail.com>
2016-04-01 01:36:24 -0300

    updating :P

--- react-app/app/index.js
+++ react-app/app/index.js
@@ -1,10 +1,35 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
+
+var ClickButton = React.createClass({
+    render: function() {
+        return <button onClick={this.props.incrementClick}>Click me</button>
+    }
+});
+
+
+var ClicksCount = React.createClass({
+    render: function() {
+        return <p>You've clicked {this.props.clicks} time(s).</p>
+    }
+});
+
 var HelloWorld = React.createClass({
+    getInitialState: function() {
+        return {clicks: 0};
+    },
+
+    incrementClick: function() {
+        this.setState({clicks: this.state.clicks + 1});
+    },
+
     render: function() {
         return (
-            <div>Hello World</div>
+            <div>
+                <ClickButton incrementClick={this.incrementClick} />
+                <ClicksCount clicks={this.state.clicks} />
+            </div>
         )
     }
 });
